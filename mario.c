dfc13fec5e9a21eed41730a0000f5697c94737ab
ioanaroman-ro <ioanaroman-ro@users.noreply.github.com>
2020-07-09 15:52:31 +0000

    automated commit by check50 [check50=True]

---
+++ mario.c
@@ -0,0 +1,45 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    //Get pyramid height
+    int height = get_int("How high would you like Mario to jump?\n");
+    //Check pyramid height limits
+    do
+    {
+       if (height < 1)
+       {
+         printf("That's too low! \n");
+         height = get_int("How high would you like Mario to jump?\n");
+       }
+       else
+       {
+          if (height > 8)
+          {
+            printf("That's too high! \n");
+            height = get_int("How high would you like Mario to jump?\n");
+          }
+       }
+   }
+  //Print pyramid
+   while (height < 1 || height > 8);
+   int i, j, spaces = 0;
+   for (i = 1; i <= height; i++)
+   {
+      for (spaces = 1; spaces <= height - i; spaces++)
+      {
+            printf(" ");
+      }
+      for (j = 1; j <= i; j++)
+      {
+         printf("#");
+      }
+        printf(" ");
+      for (j = i; j >= 1; j--)
+      {
+         printf("#");
+      }
+      printf("\n");
+   }
+}
