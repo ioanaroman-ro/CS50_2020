5bf34748b2c6a17bb039a7001d68fefd8d2643c3
ioanaroman-ro <ioanaroman-ro@users.noreply.github.com>
2020-07-09 15:31:21 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -1,30 +1,41 @@
+// Mario less pset1
 #include <cs50.h>
 #include <stdio.h>
 
 int main(void)
 {
-   int height = get_int("How high would you like Mario to jump?\n");
-   do {
-       if (height<1) {
-      printf("That's too low! \n");
-      height = get_int("How high would you like Mario to jump?\n");
+    //Get pyramid height
+    int height = get_int("How high would you like Mario to jump?\n");
+    //Check pyramid height limits
+    do
+    {
+       if (height < 1)
+       {
+         printf("That's too low! \n");
+         height = get_int("How high would you like Mario to jump?\n");
+       }
+       else
+       {
+          if (height > 8)
+          {
+            printf("That's too high! \n");
+            height = get_int("How high would you like Mario to jump?\n");
+          }
+       }
    }
-   else {
-      if (height>8) {
-      printf("That's too high! \n");
-    height = get_int("How high would you like Mario to jump?\n");
-      }
-   }
-   }
-  while (height<1 || height>8);
+  //Print pyramid
+   while (height < 1 || height > 8);
    int i, j, spaces = 0;
-   for (i = 1; i <= height; i++) {
-      for (spaces = 1; spaces <= height - i; spaces++) {
-         printf(" ");
+   for (i = 1; i <= height; i++)
+   {
+      for (spaces = 1; spaces <= height - i; spaces++)
+      {
+            printf(" ");
       }
-      for (j = 1; j <= i; j++) {
+      for (j = 1; j <= i; j++)
+      {
          printf("#");
       }
       printf("\n");
    }
- }
+}
