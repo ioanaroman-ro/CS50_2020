f4313f53a707155432c656b30f5f89342983bba7
ioanaroman-ro <ioanaroman-ro@users.noreply.github.com>
2020-07-13 16:43:28 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -7,19 +7,20 @@ int main(void)
     //Get pyramid height
     int height = get_int("How high would you like Mario to jump?\n");
     //Check pyramid height limits
-    do{
+    do
+    {
         if (height < 1)
         {
-        printf("That's too low! \n");
-        height = get_int("How high would you like Mario to jump?\n");
+            printf("That's too low! \n");
+            height = get_int("How high would you like Mario to jump?\n");
         }
         else
         {
-        if (height > 8)
-        {
-        printf("That's too high! \n");
-        height = get_int("How high would you like Mario to jump?\n");
-        }
+            if (height > 8)
+            {
+                printf("That's too high! \n");
+                height = get_int("How high would you like Mario to jump?\n");
+            }
         }
     }
     while (height < 1 || height > 8);
@@ -27,19 +28,21 @@ int main(void)
     int i, j, spaces = 0;
     for (i = 1; i <= height; i++)
     {
-    for (spaces = 1; spaces <= height - i; spaces++)
-    {
-    printf(" ");
-    }
-    for (j = 1; j <= i; j++)
-    {
-    printf("#");
-    };
-    printf("  ");
-    for (j = i; j >=1 ; j--)
-    {
-    printf("#");
-    }
-    printf("\n");
+        //First pyramid
+        for (spaces = 1; spaces <= height - i; spaces++)
+        {
+            printf(" ");
+        }
+        for (j = 1; j <= i; j++)
+        {
+            printf("#");
+        };
+        printf("  ");
+        //Reverse pyramid
+        for (j = i; j >= 1 ; j--)
+        {
+            printf("#");
+        }
+        printf("\n");
     }
 }
