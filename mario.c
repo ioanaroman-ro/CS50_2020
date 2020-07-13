e5fde8c4636123fae33d7895e933c1d59644f7ce
ioanaroman-ro <ioanaroman-ro@users.noreply.github.com>
2020-07-13 16:22:20 +0000

    automated commit by check50 [check50=True]

--- mario.c
+++ mario.c
@@ -1,29 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int correct(int height);
 int main(void)
 {
     //Get pyramid height
     int height = get_int("How high would you like Mario to jump?\n");
     //Check pyramid height limits
-    do
-    {
-       if (height < 1)
-       {
-         printf("That's too low! \n");
-         height = get_int("How high would you like Mario to jump?\n");
-       }
-       else
-       {
-          if (height > 8)
-          {
-            printf("That's too high! \n");
-            height = get_int("How high would you like Mario to jump?\n");
-          }
-       }
-   }
-  //Print pyramid
-    while (height < 1 || height > 8);
+    if (correct(height)) {
+    //Print pyramid
     int i, j, spaces = 0;
     for (i = 1; i <= height; i++)
     {
@@ -36,11 +21,32 @@ int main(void)
         printf("#");
         }
         printf(" ");
-        printf(" ");
         for (j = i; j >=1 ; j--)
         {
         printf("#");
         }
         printf("\n");
    }
+    }
+
 }
+int correct(height)
+   {
+       do{
+          if (height < 1)
+       {
+         printf("That's too low! \n");
+         height = get_int("How high would you like Mario to jump?\n");
+       }
+       else
+       {
+          if (height > 8)
+          {
+            printf("That's too high! \n");
+            height = get_int("How high would you like Mario to jump?\n");
+          }
+       }
+       }
+       while (height < 1 || height > 8);
+       return 1;
+              }
