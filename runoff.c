b387d7c4c718e68a0b83d7eefbd38162cec239d5
ioanaroman-ro <ioanaroman-ro@users.noreply.github.com>
2020-08-20 17:26:01 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -169,7 +169,7 @@ void tabulate(void)
 // Print the winner of the election, if there is one
 bool print_winner(void)
 {
-    float w = candidate_count/2;
+    float w = voter_count/2;
     for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].votes > w )
