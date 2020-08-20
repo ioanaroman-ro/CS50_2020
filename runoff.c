3fc75eda47aa69095b4d8ee02b49ae44c1887598
ioanaroman-ro <ioanaroman-ro@users.noreply.github.com>
2020-08-20 17:46:44 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -199,6 +199,10 @@ bool is_tie(int min)
     }
     if (k > 1)
     {
+        if (k < candidate_count)
+        {
+            return false;
+        }
         return true;
     }
     return false;
