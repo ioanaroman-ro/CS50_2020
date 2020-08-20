f84b7508340331054fca69eb40b5a854c55e143f
ioanaroman-ro <ioanaroman-ro@users.noreply.github.com>
2020-08-20 17:40:08 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -189,7 +189,11 @@ int find_min(void)
 // Return true if the election is tied between all candidates, false otherwise
 bool is_tie(int min)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if ((candidates[i].votes == min) && (candidates[i].eliminated == false))
+        return true;
+    }
     return false;
 }
 
