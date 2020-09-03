5cda1996d8331b3896c1705647ed763c519c5284
ioanaroman-ro <ioanaroman-ro@users.noreply.github.com>
2020-09-03 16:01:06 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -21,6 +21,42 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
+    int nr = 0;
+    int ng = 0;
+    int nb = 0;
+    for (int i = 0; i < (height - 1); i++)
+    {
+        for (int j = 0; j < (width - 1); j++)
+        {
+            nr = round(image[i][j].rgbtRed * 0.393 + image[i][j].rgbtGreen * 0.769 + image[i][j].rgbtBlue * 0.189);
+            ng = round(image[i][j].rgbtRed * 0.349 + image[i][j].rgbtGreen * 0.686 + image[i][j].rgbtBlue * 0.168);
+            nb = round(image[i][j].rgbtRed * 0.272 + image[i][j].rgbtGreen * 0.534 + image[i][j].rgbtBlue * 0.131);
+            if (nr > 255)
+            {
+                image[i][j].rgbtRed = 255;
+            }
+            else
+            {
+                image[i][j].rgbtRed = nr;
+            }
+            if (ng > 255)
+            {
+                image[i][j].rgbtGreen = 255;
+            }
+            else
+            {
+                image[i][j].rgbtGreen = ng;
+            }
+            if (nb > 255)
+            {
+                image[i][j].rgbtBlue = 255;
+            }
+            else
+            {
+                image[i][j].rgbtBlue = nr;
+            }
+        }
+    }
     return;
 }
 
