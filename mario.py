63a663e26c1a0d386740f540d8816552ca2bcca1
ioanaroman-ro <ioanaroman-ro@users.noreply.github.com>
2020-10-12 15:03:39 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,13 +1,9 @@
 from cs50 import get_int
 
 height = get_int("Height: \n")
-
 while height <= 0 or height > 8:
     height = get_int("Height: \n")
-
 i = 0
-j = 0
-spaces = 0
 
 while i < height:
     i += 1
