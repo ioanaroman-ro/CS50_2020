28f86777e2bf528eea1d62bb1df9e2bca64ccae9
ioanaroman-ro <ioanaroman-ro@users.noreply.github.com>
2020-10-12 15:28:03 +0000

    automated commit by submit50

---
+++ mario.py
@@ -0,0 +1,12 @@
+from cs50 import get_int
+
+height = get_int("Height: \n")
+i = 0
+
+while height < 1 or height > 8:
+    height = get_int("Height: \n")
+while i < height:
+    i += 1
+    print(" " * (height - i) + "#" * i + "  ", end = "")
+    print("#" * i, end = "")
+    print()
