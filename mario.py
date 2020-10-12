6f6d3e36c5bee3f9b2dcffaa94f4e6e9083e1499
ioanaroman-ro <ioanaroman-ro@users.noreply.github.com>
2020-10-12 15:33:09 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,11 +1,14 @@
 from cs50 import get_int
 
+#get user height
 height = get_int("Height: \n")
 i = 0
 
+#check user height between 1 and 8 inclusive
 while height < 1 or height > 8:
     height = get_int("Height: \n")
 
+#print pyramid
 while i < height:
     i += 1
     print(" " * (height - i) + "#" * i + "  ", end = "")
