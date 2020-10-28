04fc385a9ef5d721a973c35ae0c473757ee10f58
ioanaroman-ro <ioanaroman-ro@users.noreply.github.com>
2020-10-28 20:44:14 +0000

    automated commit by submit50

---
+++ 13.sql
@@ -0,0 +1,3 @@
+SELECT DISTINCT name FROM people JOIN stars ON people.id = stars.person_id WHERE
+movie_id IN ( SELECT movie_id FROM stars WHERE person_id in (SELECT id FROM people WHERE (name = "Kevin Bacon" AND birth = 1958)))
+AND NOT name = "Kevin Bacon";
